6939e3ecd3d39de2dbc875ee555f924afd613d9d
hsk-kr <hsk.coder@gmail.com>
2024-06-02 19:05:01 +0100

    show buttons when hovering on TaskCard

--- renderer/components/TaskCardList/index.tsx
+++ renderer/components/TaskCardList/index.tsx
@@ -1,5 +1,6 @@
-import { useMemo } from 'react';
 import { Task } from '../../types/task';
+import { IoIosOpen } from 'react-icons/io';
+import { MdDelete } from 'react-icons/md';
 
 interface TaskCardProps {
   taskName: string;
@@ -24,7 +25,7 @@ const TaskCardList = ({ tasks }: { tasks: Task[] }) => {
 
 const TaskCard = ({ data, taskName, taskType }: TaskCardProps) => {
   return (
-    <div className="w-full min-h-[140px] h-[140px] rounded-lg bg-neutral flex flex-col gap-4 p-4 box-border">
+    <div className="w-full min-h-[140px] h-[140px] rounded-lg bg-neutral flex flex-col gap-4 p-4 box-border relative hover:cursor-pointer group">
       <span className="break-words line-clamp-2 font-bold">{taskName}</span>
       <div className="flex flex-col gap-2">
         {data && (
@@ -33,6 +34,16 @@ const TaskCard = ({ data, taskName, taskType }: TaskCardProps) => {
           </span>
         )}
         <span className="badge badge-ghost uppercase">{taskType}</span>
+      </div>
+      <div className="absolute top-0 bottom-0 left-0 right-0 bg-[#00000050] justify-center items-center flex opacity-0 group-hover:opacity-100 transition-all">
+        <div className="flex gap-1">
+          <button className="btn btn-sm btn-accent text-white">
+            <IoIosOpen />
+          </button>
+          <button className="btn btn-sm btn-error text-white">
+            <MdDelete />
+          </button>
+        </div>
       </div>
     </div>
   );
